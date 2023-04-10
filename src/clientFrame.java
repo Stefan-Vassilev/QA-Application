a58f9f351b6a2c6452e397824d3e19a486d9872b
payth1 <59085690+remoski@users.noreply.github.com>
2023-04-10 17:40:44 +0200

    Added one panel for question posts

--- src/clientFrame.java
+++ src/clientFrame.java
@@ -5,11 +5,13 @@ import static javax.swing.BorderFactory.*;
 
 public class clientFrame {
 
+    JPanel individualPostPanel;
     public clientFrame() {
         //frame
         JFrame frame = new JFrame();
         frame.setSize(1300,800);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
 
 //Create posts and Hostname
         JPanel generalPanel = new JPanel();
@@ -30,13 +32,41 @@ public class clientFrame {
         frame.add(generalPanel, BorderLayout.NORTH);
 
 //Question posts Section
-
+        //Parent panel
         JPanel postPanel = new JPanel();
-        postPanel.setSize(800, 200);
-        postPanel.setBorder(createEmptyBorder(500, 30, 0, 10));
-        postPanel.setBorder(createEtchedBorder());
-
         frame.add(postPanel, BorderLayout.CENTER);
+        postPanel.setSize(1300,654);
+        postPanel.setBorder(createEtchedBorder());
+        postPanel.setLayout(new BoxLayout(postPanel, BoxLayout.Y_AXIS));
+
+        //Individual posts
+
+        individualPostPanel = new JPanel();
+
+
+        postPanel.add(individualPostPanel);
+
+        individualPostPanel.setSize(1000,120);
+        individualPostPanel.setBackground(Color.black);
+
+
+//        class DisplayPosts extends clientFrame{
+//            public DisplayPosts(){
+//
+//            }
+//        }
+
+
+        //Viewing more posts
+        JPanel arrowPanel = new JPanel();
+        JButton backButton = new JButton();
+            backButton.setText("Previous");
+        JButton forwardButton = new JButton();
+            forwardButton.setText("Next");
+        arrowPanel.add(backButton);
+        arrowPanel.add(forwardButton);
+
+        frame.add(arrowPanel, BorderLayout.SOUTH);
 
 
         frame.setVisible(true);
